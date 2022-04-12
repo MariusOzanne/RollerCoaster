e039ca582823aa7c715f9530f4d9c9fb6e2f4e6f
desktop <fquach@gaming.tech>
2022-04-12 03:53:06 +0200

    debut gameplay

--- assets/tilesets/pixel_platform_01_tileset_final.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="pixel_platform_01_tileset_final" tilewidth="32" tileheight="32" tilecount="160" columns="16">
- <image source="../Pixel_Platformer_Tileset_Pack/Asset_01/pixel_platform_01_tileset_final.png" width="512" height="320"/>
-</tileset>
--- js/MainScene.js
+++ js/MainScene.js
@@ -1,44 +1,147 @@
-import Player from "./Player.js";
-
-export default class MainScene extends Phaser.Scene {
-    constructor(){
-        super("MainScene");
+const config = {
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 1000,
+    heigth: 640,
+    scale: {
+      mode: Phaser.Scale.RESIZE,
+      autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: {
+      preload,
+      create,
+      update,
+    },
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 500 },
+      },
     }
+  };
+  
+  const game = new Phaser.Game(config);
+  
+  function preload() {
 
-    preload() {
-        Player.preload(this);
-        // this.load.image('tiles','assets/tilesets/Solaria.png');
-        // this.load.tilemapTiledJSON('map','assets/tilemaps/map.json');
-        
-        // this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
-        // this.load.tilemapTiledJSON('map', 'assets/tilemaps/game.json');
+    this.load.image('background', 'assets/images/background.png');
+
+    this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
+
+    this.load.image('spike', 'assets/images/spike.png');
+
+    this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.json');
+
+    this.load.atlas('player', 'assets/images/kenney_player.png',
+      'assets/images/kenney_player_atlas.json');
+  }
+  
+  function create() {
+
+    const map = this.make.tilemap({ key: 'map' });
+
+    const tileset = map.addTilesetImage('kenney_simple_platformer', 'tiles');
+
+    const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
+
+    backgroundImage.setScale(10, 1.8);
+ 
+    const platforms = map.createStaticLayer('Platforms', tileset, 0, 200);
+    const water = map.createStaticLayer('Water', tileset, -1025, 1225);
+ 
+    platforms.setCollisionByExclusion(-1, true);
+  
+   
+    this.player = this.physics.add.sprite(50, 1400, 'player');
+    this.player.setBounce(0.1); 
+    this.player.setCollideWorldBounds(true); 
+    this.physics.add.collider(this.player, platforms);
+    this.player.setScale(0.5, 0.5);
+ 
+    this.anims.create({
+      key: 'walk',
+      frames: this.anims.generateFrameNames('player', {
+        prefix: 'robo_player_',
+        start: 2,
+        end: 3,
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+  
+    this.anims.create({
+      key: 'idle',
+      frames: [{ key: 'player', frame: 'robo_player_0' }],
+      frameRate: 10,
+    });
+
+    this.anims.create({
+      key: 'jump',
+      frames: [{ key: 'player', frame: 'robo_player_1' }],
+      frameRate: 10,
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  
+    this.spikes = this.physics.add.group({
+      allowGravity: false,
+      immovable: true
+    });
+  
+    map.getObjectLayer('Spikes').objects.forEach((spike) => {
+      const spikeSprite = this.spikes.create(spike.x, spike.y + 200 - spike.height, 'spike').setOrigin(0);
+      spikeSprite.body.setSize(spike.width, spike.height - 20).setOffset(0, 20);
+    });
+  
+    this.physics.add.collider(this.player, this.spikes, playerHit, null, this);
+  }
+  
+  function update() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-200);
+      if (this.player.body.onFloor()) {
+        this.player.play('walk', true);
+      }
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(200);
+      if (this.player.body.onFloor()) {
+        this.player.play('walk', true);
+      }
+    } else {
+      this.player.setVelocityX(0);
+      if (this.player.body.onFloor()) {
+        this.player.play('idle', true);
+      }
     }
-
-    create() {
-        // this.add.image(0, 0,'tiles') 
-    /*     const map = this.make.tilemap({ key: 'map' });
-        const tileset = map.addTilesetImage('Solaria', 'tiles');
-        const Ground = map.createStaticLayer('Ground', tileset, 0, 200);
-        Ground.setCollisionByExclusion(-1, true); */
-        // map.createStaticLayer('Ground',tileset);
-        // map.createStaticLayer('Water',tileset);
-        // map.createStaticLayer('Wall',tileset);
-
-        // const map = this.make.tilemap({ key: 'map' });
-        // const tileset = map.addTilesetImage('platformPack_tilesheet', 'tiles');
-        // const platforms = map.createStaticLayer('platforms', tileset, 0, 200);
-
-        this.player = new Player({scene:this,x:0,y:0,texture:'male_character',frame:'walking1'});
-        let textPlayer = new Player({scene:this,x:100,y:100,texture:'male_character',frame:'walking1'});
-        this.player.inputKeys = this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.Z,
-            down: Phaser.Input.Keyboard.KeyCodes.S,
-            left: Phaser.Input.Keyboard.KeyCodes.Q,
-            right: Phaser.Input.Keyboard.KeyCodes.D,
-        })
+    if ((this.cursors.space.isDown || this.cursors.up.isDown) && this.player.body.onFloor()) {
+      this.player.setVelocityY(-450);
+      this.player.play('jump', true);
     }
-
-    update(){
-        this.player.update();
+  
+    if (this.player.body.velocity.x > 0) {
+      this.player.setFlipX(false);
+    } else if (this.player.body.velocity.x < 0) {
+      this.player.setFlipX(true);
     }
-}
+  }
+  
+  /**
+   * playerHit resets the player's state when it dies from colliding with a spike
+   * @param {*} player - player sprite
+   * @param {*} spike - spike player collided with
+   */
+  function playerHit(player, spike) {
+    player.setVelocity(0, 0);
+    player.setX(50);
+    player.setY(300);
+    player.play('idle', true);
+    player.setAlpha(0);
+    let tw = this.tweens.add({
+      targets: player,
+      alpha: 1,
+      duration: 100,
+      ease: 'Linear',
+      repeat: 5,
+    });
+  }
+  
